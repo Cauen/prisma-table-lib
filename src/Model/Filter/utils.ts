347edf6771f3dfa07eda6999a5a972f16dc2ffaa
Emanuel <emanuel-caue@hotmail.com>
2023-02-24 15:40:43 -0300

    fix: fix build

--- src/Model/Filter/utils.ts
+++ src/Model/Filter/utils.ts
@@ -1,5 +1,5 @@
 export function setGetParam(key: string, value: string) {
-    if (history.pushState) {
+    if (typeof history.pushState !== "undefined") {
         const params = new URLSearchParams(window.location.search);
         params.set(key, value);
         const newUrl =
